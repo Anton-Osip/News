05f3747e8cff87be81401505daea02cb2fbc4997
Anton <osipchika@gmail.com>
2020-08-13 10:22:45 +0300

    сверстал полностью

--- build/js/main.js
+++ build/js/main.js
@@ -14,3 +14,41 @@ var mySwiper = new Swiper('.swiper-container', {
      },
 
 })
+
+var sliderBottom = new Swiper('.slider-bottom__container', {
+     pagination: {
+          el: '.slider-bottom-pagination',
+     },
+     navigation: {
+          nextEl: '.slider-bottom__btn-next',
+          prevEl: '.slider-bottom__btn-prev',
+     },
+     breakpoints: {
+          240: {
+               slidesPerView: 1,
+               spaceBetween: 30,
+               loop: true,
+          },
+          540: {
+               slidesPerView: 4,
+               spaceBetween: 30,
+               loop: true,
+
+          },
+     }
+})
+
+
+// MENU
+
+let menu = document.querySelector('.menu');
+let cross = document.querySelector('.header__link--cross');
+let headerList = document.querySelector('.header__list');
+
+
+menu.addEventListener('click', () => {
+     headerList.classList.add('header__list__is-open');
+})
+cross.addEventListener('click', () => {
+     headerList.classList.remove('header__list__is-open');
+})
